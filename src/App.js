e1c457f75b3d6aa466eb20797c15e9b7619ae80e
rastuszek <skorupa7@o2.pl>
2025-10-07 03:13:49 +0200

    update deploy3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // App.js
 
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
