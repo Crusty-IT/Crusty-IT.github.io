c2898b8423f44ccd14416dde8c7c1e4ee6f4828a
Tomasz Skorupski <skorupa7@o2.pl>
2026-02-13 02:09:05 +0100

    rebrand continues

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./Home.css";
 import { HashLink } from 'react-router-hash-link';
 import { useTranslation } from "react-i18next";
-import LogoSVG from "./assets/logo_vertical";
+import LogoSVG from "./assets/logo_vertical1";
 
 const ROLES_PL = [
     "Specjalista IT",
